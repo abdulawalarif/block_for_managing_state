227b764a878b24ed14e82829a9fee2d84e235349
abdul awal <awal@twintechsoft.com>
2023-10-02 01:40:04 +0600

    data fetched from network using cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:block_for_managing_state/cubit/user_cubit.dart';
+import 'package:block_for_managing_state/cubit/user_cubit_state.dart';
+import 'package:block_for_managing_state/model/user_model.dart';
 import 'package:block_for_managing_state/service/api_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -42,7 +44,30 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      body: Center(child: Text("Hello world",style: textStyle,),),
+      appBar: AppBar(
+        title: Text("Cubit API Calling"),
+      ),
+      body: BlocBuilder<UserCubit, UserCubitState>(
+        builder: (context, state){
+          if(state is UserCubitLoading){
+            return Center(child: CircularProgressIndicator(),);
+          } else if(state is UserCubitDataFetchingError){
+            return Center(child: Text(state.errorMessage.toString()),);
+          } else if(state is UserCubitDataLoaded){
+            return ListView.builder(
+                itemCount: state.userList.length,
+                itemBuilder: (context, index){
+                  UserModel userModel = state.userList[index];
+              return ListTile(
+                leading: Text(userModel.id.toString()),
+                title: Text(userModel.name.toString()),
+              );
+            });
+          }else{
+            return Text("Unknown error occured");
+          }
+        },
+      )
     );
   }
 }
