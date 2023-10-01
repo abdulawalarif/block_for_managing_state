e6ba011b553948144f54e205313cd90353c7ddf9
abdul awal <awal@twintechsoft.com>
2023-10-01 22:22:15 +0600

    counter app with cubit is working

--- lib/main.dart
+++ lib/main.dart
@@ -1,160 +1,48 @@
-import 'package:bloc/bloc.dart';
+import 'package:block_for_managing_state/cubit/counter_cubit.dart';
+import 'package:block_for_managing_state/cubit/counter_cubit_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false ,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage(),
-    ),
-  );
+  runApp(MyApp());
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  late final TextEditingController _controller;
-
-  @override
-  void initState() {
-    _controller = TextEditingController();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
+class MyApp extends StatelessWidget {
+    MyApp({super.key});
+ final textStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold, );
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => CounterBloc(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Testing bloc'),
-        ),
-        body: BlocConsumer<CounterBloc, CounterState>(
-          listener: (context, state) {
-            _controller.clear();
-          },
-          builder: (context, state) {
-            final invalidValue =
-                (state is CounterStateInvalidNumber) ? state.invalidValue : '';
-            return Column(
-              children: [
-                Text('Current value => ${state.value}'),
-                Visibility(
-                  child: Text('Invalid input: $invalidValue'),
-                  visible: state is CounterStateInvalidNumber,
-                ),
-                TextField(
-                  controller: _controller,
-                  decoration: const InputDecoration(
-                    hintText: 'Enter a number here',
-                  ),
-                  keyboardType: TextInputType.number,
-                ),
-                Row(
-                  children: [
-                    TextButton(
-                      onPressed: () {
-                        context
-                            .read<CounterBloc>()
-                            .add(DecrementEvent(_controller.text));
-                      },
-                      child: const Text('-'),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        context
-                            .read<CounterBloc>()
-                            .add(IncrementEvent(_controller.text));
-                      },
-                      child: const Text('+'),
-                    ),
-                  ],
-                )
-              ],
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
+    return MultiBlocProvider(
+        providers: [BlocProvider(create: (context) => CounterCubit())],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(),
+          home: Scaffold(
+              body: BlocBuilder<CounterCubit, CounterCubitState>(
+                builder: (contex, state){
+                  return  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          IconButton(onPressed: () {
+                            contex.read<CounterCubit>().counterDecrement();
+                          }, icon: Icon(Icons.remove)),
+                          (state is CounterValueUpdated) ?Text(state.counter.toString(), style: textStyle,):Text("0", style: textStyle),
+                          IconButton(onPressed: () {
+                            contex.read<CounterCubit>().counterIncrement();
+                          }, icon: Icon(Icons.add)),
+                        ],
+                      )
+                    ],
+                  );
+                }
+              )
 
-@immutable
-abstract class CounterState {
-  final int value;
-  const CounterState(this.value);
-}
 
-class CounterStateValid extends CounterState {
-  const CounterStateValid(int value) : super(value);
-}
-
-class CounterStateInvalidNumber extends CounterState {
-  final String invalidValue;
-  const CounterStateInvalidNumber({
-    required this.invalidValue,
-    required int previousValue,
-  }) : super(previousValue);
-}
-
-@immutable
-abstract class CounterEvent {
-  final String value;
-  const CounterEvent(this.value);
-}
-
-class IncrementEvent extends CounterEvent {
-  const IncrementEvent(String value) : super(value);
-}
-
-class DecrementEvent extends CounterEvent {
-  const DecrementEvent(String value) : super(value);
-}
-
-class CounterBloc extends Bloc<CounterEvent, CounterState> {
-  CounterBloc() : super(const CounterStateValid(0)) {
-    on<IncrementEvent>((event, emit) {
-      final integer = int.tryParse(event.value);
-      if (integer == null) {
-        emit(
-          CounterStateInvalidNumber(
-            invalidValue: event.value,
-            previousValue: state.value,
           ),
-        );
-      } else {
-        emit(CounterStateValid(state.value + integer));
-      }
-    });
-    on<DecrementEvent>((event, emit) {
-      final integer = int.tryParse(event.value);
-      if (integer == null) {
-        emit(
-          CounterStateInvalidNumber(
-            invalidValue: event.value,
-            previousValue: state.value,
-          ),
-        );
-      } else {
-        emit(CounterStateValid(state.value - integer));
-      }
-    });
+        ));
   }
 }
