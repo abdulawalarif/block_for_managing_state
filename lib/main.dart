11f6787315b2575ece25f4327045ef5c90c35d70
abdul awal <awal@twintechsoft.com>
2023-11-21 12:21:19 +0600

    Persons bloc is implemented

--- lib/main.dart
+++ lib/main.dart
@@ -62,26 +62,46 @@ class Person {
 }
 
 Future<Iterable<Person>> getPersons(String url) => HttpClient()
-.getUrl(Uri.parse(url))
-.then((req) =>req.close())
-.then((response) => response.transform(utf8.decoder).join())
-.then((str) => json.decode(str) as List<dynamic>)
-.then((list) => list.map((e) => Person.fromJson(e)));
+    .getUrl(Uri.parse(url))
+    .then((req) => req.close())
+    .then((response) => response.transform(utf8.decoder).join())
+    .then((str) => json.decode(str) as List<dynamic>)
+    .then((list) => list.map((e) => Person.fromJson(e)));
 
 @immutable
 class FetchResult {
-   final Iterable<Person> persons;
-   final isRetrievedFromCache;
-   const FetchResult({
+  final Iterable<Person> persons;
+  final bool isRetrievedFromCache;
+  const FetchResult({
     required this.persons,
     required this.isRetrievedFromCache,
-   }); 
+  });
 
-   @override
-   String toString()=>'FetchResult (isRetrivedFromCache = $isRetrievedFromCache, persons = $persons)';
-
+  @override
+  String toString() =>
+      'FetchResult (isRetrivedFromCache = $isRetrievedFromCache, persons = $persons)';
 }
 
+class PersonsBloc extends Bloc<LoadAction, FetchResult?> {
+  final Map<PersonUrl, Iterable<Person>> _cache = {};
+  PersonsBloc() : super(null) {
+    on<LoadPersonsAction>((event, emit) async {
+      final url = event.url;
+      if (_cache.containsKey(url)) {
+        //we have the value in the cache
+        final cachedPersons = _cache[url]!;
+        final result =
+            FetchResult(persons: cachedPersons, isRetrievedFromCache: true);
+        emit(result);
+      } else {
+        final persons = await getPersons(url.urlString);
+        _cache[url] = persons;
+        final result =
+            FetchResult(persons: persons, isRetrievedFromCache: false,);
+      }
+    });
+  }
+}
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
