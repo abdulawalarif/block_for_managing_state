97d50e00ee76b72a47dc739f88ddf091203e5630
abdul awal <awal@twintechsoft.com>
2024-02-17 00:57:07 +0600

    contact singletone

--- lib/main.dart
+++ lib/main.dart
@@ -1,110 +1,68 @@
-import 'dart:convert';
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'dart:developer' as devtools show log;
-import 'bloc/bloc_actions.dart';
-import 'bloc/person.dart';
-import 'bloc/persons_bloc.dart';
 
-
-
-///2:02:16 / 11:29:38
-extension Log on Object {
-  void log() => devtools.log(toString());
-}
-
+/// 7:05 / 1:01:59
 void main() {
   runApp(
     MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: BlocProvider(
-        create: (_) => PersonsBloc(),
-        child: const HomePage(),
-      ),
-    ),
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen()),
   );
 }
 
-Future<Iterable<Person>> getPersons(String url) => HttpClient()
-    .getUrl(Uri.parse(url))
-    .then((req) => req.close())
-    .then((resp) => resp.transform(utf8.decoder).join())
-    .then((str) => json.decode(str) as List<dynamic>)
-    .then((list) => list.map((e) => Person.fromJson(e)));
-
-extension Subscript<T> on Iterable<T> {
-  T? operator [](int index) => length > index ? elementAt(index) : null;
+class Contact {
+  final String name;
+  Contact({
+    required this.name,
+  });
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class ContactBook {
+  ContactBook._sharedInstance();
+  static final ContactBook _shared = ContactBook._sharedInstance();
+  factory ContactBook() => _shared;
+  final List<Contact> _contacts = [];
+  int get length => _contacts.length;
+
+  void add({required Contact contact}) {
+    _contacts.add(contact);
+  }
+
+  void remove({required Contact contact}) {
+    _contacts.remove(contact);
+  }
+
+  Contact? contact({required int indexAt}) =>
+      length > indexAt ? _contacts[indexAt] : null;
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    final contactBook = ContactBook();
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Home Page"),
-        ),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Row(
-                children: [
-                  TextButton(
-                    onPressed: () {
-                      context.read<PersonsBloc>().add(
-                            const LoadPersonsAction(
-                                url: personOneUrl, personLoader: getPersons),
-                          );
-                    },
-                    child: const Text("Load Json #1"),
-                  ),
-                  Spacer(),
-                  TextButton(
-                    onPressed: () {
-                      context.read<PersonsBloc>().add(
-                            const LoadPersonsAction(
-                                url: personTwoUrl, personLoader: getPersons),
-                          );
-                    },
-                    child: const Text("Load Json #2"),
-                  ),
-                ],
-              ),
-            ),
-            BlocBuilder<PersonsBloc, FetchResult?>(
-                buildWhen: (previousResult, currentResult) {
-              return previousResult?.persons != currentResult?.persons;
-            }, builder: (context, fetchResult) {
-              fetchResult?.log();
-              final persons = fetchResult?.persons;
-              if (persons == null) {
-                return const SizedBox();
-              }
-              return Expanded(
-                child: ListView.builder(
-                  itemCount: persons.length,
-                  itemBuilder: (context, index) {
-                    final person = persons[index];
-                    return ListTile(
-                      title: Text(person?.name ?? ''),
-                    );
-                  },
-                ),
-              );
-            })
-          ],
-        ));
+      appBar: AppBar(
+        title: const Text("Contact app"),
+      ),
+      body: ListView.builder(
+        itemCount: contactBook.length,
+        itemBuilder: (context, index) {
+          final contact = contactBook.contact(indexAt: index)!;
+          return ListTile(
+            title: Text(contact.name),
+          );
+        },
+      ),
+    );
   }
 }
