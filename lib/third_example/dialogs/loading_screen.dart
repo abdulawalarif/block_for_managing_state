aa6502d7e3677031941cf6e254e314dd7c35fa92
abdul awal <awal@twintechsoft.com>
2024-04-20 18:02:41 +0600

    loading screen is done

--- lib/third_example/dialogs/loading_screen.dart
+++ lib/third_example/dialogs/loading_screen.dart
@@ -1,6 +1,55 @@
+import 'dart:async';
+import 'package:block_for_managing_state/third_example/dialogs/loading_screen_controller.dart';
+import 'package:flutter/material.dart';
+
 class LoadingScreen {
   // singleton pattern
   LoadingScreen._sharedInstance();
   static late final LoadingScreen _shared = LoadingScreen._sharedInstance();
   factory LoadingScreen.instance() => _shared;
+
+  LoadingScreenController showOverlay({
+    required BuildContext context,
+    required String text,
+  }) {
+    final _text = StreamController<String>();
+    _text.add(text);
+
+    // get the size
+
+    final state = Overlay.of(context);
+    final renderBox = context.findRenderObject() as RenderBox;
+    final size = renderBox.size;
+
+    final overlay = OverlayEntry(
+      builder: (context) {
+        return Material(
+          color: Colors.black.withAlpha(
+            150,
+          ),
+          child: Center(
+            child: Container(
+              constraints: BoxConstraints(
+                maxWidth: size.width * 0.8,
+                maxHeight: size.height * 0.0,
+                minWidth: size.width * 0.5,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                  children: [],
+                )),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
 }
