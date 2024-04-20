e5e5d4461e3409cbcc12489c706c72b4b79460a7
abdul awal <awal@twintechsoft.com>
2024-04-20 16:53:44 +0600

    loading Screen singleton

---
+++ lib/third_example/dialogs/loading_screen.dart
@@ -0,0 +1,6 @@
+class LoadingScreen {
+  // singleton pattern
+  LoadingScreen._sharedInstance();
+  static late final LoadingScreen _shared = LoadingScreen._sharedInstance();
+  factory LoadingScreen.instance() => _shared;
+}
