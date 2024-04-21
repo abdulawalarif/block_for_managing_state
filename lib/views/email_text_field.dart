6701aec50a361b9a35395a3911fc743b4360830f
abdul awal <awal@twintechsoft.com>
2024-04-21 19:34:53 +0600

    email text field is done

---
+++ lib/views/email_text_field.dart
@@ -0,0 +1,22 @@
+import 'package:block_for_managing_state/third_example/strings.dart'show enterYourEmailHere;
+import 'package:flutter/material.dart';
+
+class EmailTextField extends StatelessWidget {
+  final TextEditingController emailController;
+  const EmailTextField({
+    super.key,
+    required this.emailController,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: emailController,
+      keyboardType: TextInputType.emailAddress,
+      autocorrect: false,
+      decoration: const InputDecoration(
+        hintText: enterYourEmailHere, 
+      ),
+    );
+  }
+}
